76d16b39daaed6a0539e3f25a22d1b11f02bbaf1
Devansh Kumar Singh <devansh9452@gmail.com>
2024-01-16 04:53:10 +0530

    seperated the connection part and get part into different function

--- mongoindex.js
+++ mongoindex.js
@@ -1,7 +1,9 @@
 const {MongoClient}=require('mongodb')
 const url='mongodb://localhost:27017'
 const client=new MongoClient(url);
-let database='youtube'
+let database='youtube';
+
+
 async function getData(){
     let result =await client.connect();
     let db=result.db(database);
@@ -10,3 +12,18 @@ async function getData(){
     console.log(response);
 }
 getData();
+
+
+
+
+    // function getData(){
+    // client.connect().then((result)=>{
+    // let db=result.db(database);
+    // let collection =db.collection('videos');
+    // collection.find().toArray().then((response)=>{
+    //     console.log(response);
+    // }) 
+    // })
+    
+    
+
