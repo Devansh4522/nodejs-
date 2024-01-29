c1dec8c1f56f5ac6b80e73723c012ac25f5895eb
Devansh Kumar Singh <devansh9452@gmail.com>
2024-01-29 19:51:50 +0530

    new practice mongo

---
+++ mongoIn1.js
@@ -0,0 +1,33 @@
+const {MongoClient}=require('mongodb');
+const url='mongodb://localhost:27017';
+const client=new MongoClient(url);
+let database='databassse'
+
+async function dbConnect(){
+    let result= await client.connect();
+    let db=result.db(database)
+    return db;
+}
+
+
+// async function dbConnect(){
+//     let result= await client.connect();
+//     let db=result.db(database)
+//     let collection=db.collection('coll2');
+//     let resp= await collection.find().toArray();
+//     console.log(resp)
+
+// }
+
+
+// function dbConnect(){
+//     client.connect().then((result)=>{
+//         let db=result.db(database);
+//         let collection=db.collection('coll2');
+//         collection.find().toArray().then((resp)=>{
+//             console.log(resp);
+//         })
+//     })
+// }
+dbConnect();
+module.exports=dbConnect;
