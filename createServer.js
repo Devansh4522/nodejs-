ec3c33144e632c903a69064168a572e7863d2376
Devansh Kumar Singh <devansh9452@gmail.com>
2024-02-05 23:34:56 +0530

    cs

--- createServer.js
+++ createServer.js
@@ -1,6 +1,6 @@
 const http=require("http")
 const fs=require("fs")
-let xyz=fs.readFileSync("index.html")
+let xyz=fs.readFileSync("new/index.html")
 const server=http.createServer((req,res)=>{
     res.writeHead(200, {'Content-type':'text/html'})
     res.end(xyz)
@@ -8,3 +8,17 @@ const server=http.createServer((req,res)=>{
 server.listen(80,'127.0.0.1',()=>{
     console.log('server listening on port 80')
 })
+
+// const http=require("http")
+// const hostname='127.0.0.1'
+// const port=4000
+// const fs=require("fs")
+//  let xyz=fs.readFileSync("new/index.html")
+// const server=http.createServer((req,res)=>{
+//     res.writeHead({'Content-type':'text/html'})
+//     res.end(xyz)
+// })
+
+// server.listen(port,hostname,()=>{
+//     console.log(`listening at http://${hostname}:${port}/`)
+// })
